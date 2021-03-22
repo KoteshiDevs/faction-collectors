525c7901ce7204abf17c6cb1c2e484277ebf3225
zHeroandre <75711194+zHeroandre@users.noreply.github.com>
2021-03-22 22:54:10 +0100

    Update FactionDisbandListener.java

--- src/main/java/me/zheroandre/factioncollectors/listener/FactionDisbandListener.java
+++ src/main/java/me/zheroandre/factioncollectors/listener/FactionDisbandListener.java
@@ -5,8 +5,6 @@ import me.zheroandre.factioncollectors.FactionCollectorsPlugin;
 import me.zheroandre.factioncollectors.handler.collector.CollectorHandler;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
-import org.bukkit.event.player.PlayerInteractEvent;
 
 public class FactionDisbandListener implements Listener {
 
@@ -19,13 +17,6 @@ public class FactionDisbandListener implements Listener {
     @EventHandler
     public void factionDisbandEvent(FactionDisbandEvent e) {
         collectorHandler.delete(e.getFaction().getTag());
-    }
-
-    @EventHandler
-    public void a(PlayerInteractEvent e) {
-        if (e.getAction() == Action.RIGHT_CLICK_BLOCK) {
-            e.getClickedBlock().setType(e.getPlayer().getItemInHand().getType());
-        }
     }
 
 }
